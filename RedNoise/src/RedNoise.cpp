620826d219a6eaa1c9b72959affcc529d399ba2c
Luca <lucagough7@gmail.com>
2023-09-30 13:59:33 +0100

    Finished Week2 tasks

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -3,12 +3,44 @@
 #include <Utils.h>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+#include <glm/glm.hpp>
 
 #define WIDTH 320
 #define HEIGHT 240
 
-void draw(DrawingWindow &window) {
-	window.clearPixels();
+std::vector<float> interpolateSingleFloats(float from, float to, int steps) {
+	float step = (to - from) / (steps - 1); 
+	std::vector<float> results;
+	for (int i = 0; i < steps; i++) {
+		results.push_back(from + (step * i));
+	}
+	return results;
+}
+
+std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int steps) {
+	glm::vec3 dir = (to - from) * (1.0f / (steps - 1)); 
+	std::vector<glm::vec3> results;
+	for (int i = 0; i < steps; i++) {
+		results.push_back(from + (dir * static_cast<float>(i)));
+	}
+	return results;
+}
+
+void interpolateSingleFloatsTest() {
+	std::vector<float> result;
+	result = interpolateSingleFloats(2.2, 8.5, 7);
+	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
+	std::cout << std::endl;
+}
+
+void interpolateThreeElementsTest() {
+	std::vector<glm::vec3> result;
+	result = interpolateThreeElementValues(glm::vec3(1.0, 4.0, 9.2), glm::vec3(4.0, 1.0, 9.8), 4);
+	for (size_t i = 0; i < result.size(); i++) std::cout << result[i].r << ", " << result[i].g << ", " << result[i].b << std::endl;
+}
+
+void RedNoise(DrawingWindow &window) {
 	for (size_t y = 0; y < window.height; y++) {
 		for (size_t x = 0; x < window.width; x++) {
 			float red = rand() % 256;
@@ -18,6 +50,45 @@ void draw(DrawingWindow &window) {
 			window.setPixelColour(x, y, colour);
 		}
 	}
+}
+
+uint32_t PackColour(uint8_t r, uint8_t g, uint8_t b) {
+	return (255 << 24) + (r << 16) + (g << 8) + b;
+}
+
+void GrayscaleGradient(DrawingWindow &window) {
+	std::vector<float> row = interpolateSingleFloats(255, 0, window.width);
+	std::vector<uint32_t> rowColours;
+	std::transform(row.cbegin(), row.cend(), std::back_inserter(rowColours), [](float f) -> uint32_t { return PackColour(f, f, f); });
+	for (size_t y = 0; y < window.height; y++) {
+		for (size_t x = 0; x < window.width; x++) {
+			window.setPixelColour(x, y, rowColours.at(x));
+		}
+	}
+}
+
+void TwoDGradient(DrawingWindow &window) {
+	glm::vec3 topLeft(255, 0, 0);        // red 
+	glm::vec3 topRight(0, 0, 255);       // blue 
+	glm::vec3 bottomRight(0, 255, 0);    // green 
+	glm::vec3 bottomLeft(255, 255, 0);   // yellow
+
+	std::vector<glm::vec3> left = interpolateThreeElementValues(topLeft, bottomLeft, window.height);
+	std::vector<glm::vec3> right = interpolateThreeElementValues(topRight, bottomRight, window.height);
+	
+	for (size_t y = 0; y < window.height; y++) {
+		std::vector<glm::vec3> row = interpolateThreeElementValues(left[y], right[y], window.width);
+		for (size_t x = 0; x < window.width; x++) {
+			window.setPixelColour(x, y, PackColour(row[x].r, row[x].g, row[x].b));
+		}
+	}
+}
+
+void draw(DrawingWindow &window) {
+	window.clearPixels();
+	// RedNoise(window);
+	// GrayscaleGradient(window);
+	TwoDGradient(window);
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window) {
@@ -33,6 +104,8 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 }
 
 int main(int argc, char *argv[]) {
+	interpolateSingleFloatsTest();
+	interpolateThreeElementsTest();
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
 	while (true) {
