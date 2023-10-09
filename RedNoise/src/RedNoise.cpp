f52ba4c6c84e2a8c5cb44166f0e450839f01699d
Luca <lucagough7@gmail.com>
2023-10-09 10:32:51 +0100

    week3

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -7,32 +7,195 @@
 #include <glm/glm.hpp>
 #include <cmath>
 #include <Colour.h>
+#include <CanvasTriangle.h>
+#include <TextureMap.h>
 
 #define WIDTH 320
 #define HEIGHT 240
 
-struct Line2D {
-	std::vector<glm::vec2> points;
+struct Shape2D {
+	std::vector<CanvasPoint> points;
 	Colour colour;
-	Line2D()
-} Line2D;
+};
 
-Line2D Line2D(glm::vec2 from, glm::vec2 to, Colour c) {
+glm::vec2 CanvasPointToVec2(CanvasPoint p) {
+	return { p.x, p.y };
+}
+
+Shape2D CreateLine2D(CanvasPoint from, CanvasPoint to, Colour c) {
 	float xdiff = to.x - from.x;
 	float ydiff = to.y - from.y;
 	float steps = fmax(fabs(xdiff), fabs(ydiff));
 	float xstep = xdiff / steps;
 	float ystep = ydiff / steps;
-	std::vector<glm::vec2> ps;
+	std::vector<CanvasPoint> ps;
 	for (float i = 0.0; i < steps; i++) {
 		float x = from.x + xstep * i;
 		float y = from.y + ystep * i;
-		ps.push_back(glm::vec2(x, y));
+		ps.push_back(CanvasPoint(x, y));
 	}
-	return Line2D(ps, c);
+	return { ps, c };
 }
 
-std::vector<float> interpolateSingleFloats(float from, float to, int steps) {
+Shape2D CreateStrokedTriangle2D(CanvasTriangle verticies, Colour c) {
+	CanvasPoint v0 = verticies.v0();
+	CanvasPoint v1 = verticies.v1();
+	CanvasPoint v2 = verticies.v2();
+
+	Shape2D l1 = CreateLine2D(v0, v1, c);
+	Shape2D l2 = CreateLine2D(v1, v2, c);
+	Shape2D l3 = CreateLine2D(v2, v0, c);
+	std::vector<CanvasPoint> ps;
+	ps.insert(ps.end(), l1.points.begin(), l1.points.end());
+	ps.insert(ps.end(), l2.points.begin(), l2.points.end());
+	ps.insert(ps.end(), l3.points.begin(), l3.points.end());
+	return { ps, c };
+}
+
+Shape2D FillFlatBottomTriangle(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, Colour c) {
+	float slope1 = (v1.x - v0.x) / (v1.y - v0.y);
+	float slope2 = (v2.x - v0.x) / (v2.y - v0.y);
+	float x1 = v0.x;
+	float x2 = v0.x;
+	std::vector<CanvasPoint> ps;
+	for (int y = v0.y; y >= v1.y; y--) {
+		Shape2D l = CreateLine2D(CanvasPoint(x1, y), CanvasPoint(x2, y), Colour());
+		x1 -= slope1;
+		x2 -= slope2;
+		ps.insert(ps.begin(), l.points.begin(), l.points.end());
+	}
+	return { ps, c };
+}
+
+Shape2D FillFlatToppedTriangle(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, Colour c) {
+	float slope1 = (v2.x - v0.x) / (v2.y - v0.y);
+	float slope2 = (v2.x - v1.x) / (v2.y - v1.y);
+	float x1 = v2.x;
+	float x2 = v2.x;
+	std::vector<CanvasPoint> ps;
+	for (int y = v2.y; y <= v0.y; y++) {
+		Shape2D l = CreateLine2D(CanvasPoint(x1, y), CanvasPoint(x2, y), Colour());
+		x1 += slope1;
+		x2 += slope2;
+		ps.insert(ps.begin(), l.points.begin(), l.points.end());
+	}
+	return { ps, c };
+}
+
+void SortVerticies(CanvasTriangle &triangle) {
+	std::sort(triangle.vertices.begin(), triangle.vertices.end(), [](const CanvasPoint a, const CanvasPoint b) { return a.y > b.y; } );
+}
+
+std::pair<Shape2D, Shape2D> CreateFilledTriangle2D(CanvasTriangle verticies, Colour c) {
+	SortVerticies(verticies);
+	CanvasPoint v0 = verticies.v0();
+	CanvasPoint v1 = verticies.v1();
+	CanvasPoint v2 = verticies.v2();
+	std::vector<CanvasPoint> ps;
+	if (verticies.v1().y == verticies.v2().y) {
+		ps = FillFlatBottomTriangle(v0, v1, v2, c).points;
+	} 
+	else if (v0.y == v1.y) {
+		ps = FillFlatToppedTriangle(v0, v1, v2, c).points;
+	} 
+	else {
+		float v3x = v0.x + ((v1.y - v0.y) / (v2.y - v0.y)) * (v2.x - v0.x);
+		CanvasPoint v3 = CanvasPoint(v3x, v1.y);
+		Shape2D bottom = FillFlatBottomTriangle(v0, v1, v3, c);
+		Shape2D top = FillFlatToppedTriangle(v1, v3, v2, c);
+		ps.insert(ps.begin(), bottom.points.begin(), bottom.points.end());
+		ps.insert(ps.begin(), top.points.begin(), top.points.end());
+	}
+	Shape2D outline = CreateStrokedTriangle2D(verticies, Colour(255, 255, 255));
+	Shape2D shaded = { ps, c };
+	return std::make_pair(shaded, outline);
+}
+
+TexturePoint CanvasPointToTexturePoint(CanvasPoint p) {
+	return TexturePoint(p.x, p.y);
+}
+
+glm::vec3 CanvasPointToVec3(CanvasPoint p) {
+	return glm::vec3(p.x, p.y, 1.0);
+}
+
+std::vector<glm::vec2> InterpolateTwoElementValues(glm::vec2 from, glm::vec2 to, int steps) {
+	glm::vec2 dir = (to - from) * (1.0f / (steps - 1));
+	std::vector<glm::vec2> results;
+	for (int i = 0; i < steps; i++) {
+		results.push_back(from + (dir + static_cast<float>(i)));
+	}
+	return results;
+}
+
+Shape2D TexturedFlatBottomTriangle2D(CanvasPoint v0, CanvasPoint v1, CanvasPoint v2, CanvasPoint tv0, CanvasPoint tv1, CanvasPoint tv2) {
+	// Assume v2 at the top
+	float slope1 = (v1.x - v0.x) / (v1.y - v0.y);
+	float slope2 = (v2.x - v0.x) / (v2.y - v0.y);
+	float x1 = v0.x;
+	float x2 = v0.x;
+	std::vector<CanvasPoint> ps;
+
+	glm::vec2 diff1 = CanvasPointToVec2(v1) - CanvasPointToVec2(v0);
+	float steps1 = fmax(fabs(diff1.x), fabs(diff1.y));
+
+	glm::vec2 diff2 = CanvasPointToVec2(v2) - CanvasPointToVec2(v0);
+	float steps2 = fmax(fabs(diff2.x), fabs(diff2.y));
+
+	std::vector<glm::vec2> tl1 = InterpolateTwoElementValues(CanvasPointToVec2(tv0), CanvasPointToVec2(tv1), steps1);
+	std::vector<glm::vec2> tl2 = InterpolateTwoElementValues(CanvasPointToVec2(tv0), CanvasPointToVec2(tv2), steps2);
+	for (int y = v0.y; y >= v1.y; y--) {		
+		Shape2D l = CreateLine2D(CanvasPoint(x1, y), CanvasPoint(x2, y), Colour());
+		x1 += slope1;
+		x2 += slope2;
+		std::vector<glm::vec2> textureScanline = InterpolateTwoElementValues(glm::vec2(x1, y), glm::vec2())
+	}
+}
+
+Shape2D CreateTexturedTriangle2D(CanvasTriangle verticies, CanvasTriangle texturedTriangle) {
+	verticies.v0().texturePoint = CanvasPointToTexturePoint(texturedTriangle.v0());
+	verticies.v1().texturePoint = CanvasPointToTexturePoint(texturedTriangle.v1());
+	verticies.v2().texturePoint = CanvasPointToTexturePoint(texturedTriangle.v2());
+	CanvasPoint v0 = verticies.v0();
+	CanvasPoint v1 = verticies.v1();
+	CanvasPoint v2 = verticies.v2();
+	int maxX = fmax(v0.x, fmax(v1.x, v2.x));
+	int minX = fmin(v0.x, fmin(v1.x, v2.x));
+	int maxY = fmax(v0.y, fmax(v1.y, v2.y));
+	int minY = fmin(v0.y, fmin(v1.y, v2.y));
+	glm::vec3 A = CanvasPointToVec3(v0);
+	glm::vec3 B = CanvasPointToVec3(v1);
+	glm::vec3 C = CanvasPointToVec3(v2);
+	float area = glm::length(glm::cross((B - A), (C - A)));
+
+	std::vector<CanvasPoint> ps;
+	for (int y = minY; y <= maxY; y++) {
+		for (int x = minX; x <= maxX; x++) {
+			glm::vec3 P = CanvasPointToVec3(CanvasPoint(x, y));
+			glm::vec3 PC = C - P;
+			glm::vec3 PB = B - P;
+			glm::vec3 PA = A - P;
+			float alpha = glm::length(glm::cross(PB, PC)) / area;
+			float beta = glm::length(glm::cross(PC, PA)) / area;
+			float gamma = glm::length(glm::cross(PA, PB)) / area;
+			if ((alpha >= 0.0 && alpha <= 1.0) && (beta >= 0.0 && beta <= 1.0) && (gamma >= 0.0 && gamma <= 1.0) && fabs(alpha + beta + gamma - 1) <= 0.01) {
+				glm::vec2 textureCoord = 
+					CanvasPointToVec2(texturedTriangle.v0()) * alpha
+					+ CanvasPointToVec2(texturedTriangle.v1()) * beta
+					+ CanvasPointToVec2(texturedTriangle.v2()) * gamma;
+				CanvasPoint p = CanvasPoint(x, y);
+				if (x == 300 && y == 230) {
+					std::cout << textureCoord.x << ", " << textureCoord.y << std::endl;
+				}
+				p.texturePoint = TexturePoint(textureCoord.x, textureCoord.y);
+				ps.push_back(p);
+			}
+		}
+	}
+	return { ps, Colour() };
+}
+
+std::vector<float> InterpolateSingleFloats(float from, float to, int steps) {
 	float step = (to - from) / (steps - 1); 
 	std::vector<float> results;
 	for (int i = 0; i < steps; i++) {
@@ -41,7 +204,7 @@ std::vector<float> interpolateSingleFloats(float from, float to, int steps) {
 	return results;
 }
 
-std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int steps) {
+std::vector<glm::vec3> InterpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int steps) {
 	glm::vec3 dir = (to - from) * (1.0f / (steps - 1)); 
 	std::vector<glm::vec3> results;
 	for (int i = 0; i < steps; i++) {
@@ -50,16 +213,16 @@ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 t
 	return results;
 }
 
-void interpolateSingleFloatsTest() {
+void InterpolateSingleFloatsTest() {
 	std::vector<float> result;
-	result = interpolateSingleFloats(2.2, 8.5, 7);
+	result = InterpolateSingleFloats(2.2, 8.5, 7);
 	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
 	std::cout << std::endl;
 }
 
-void interpolateThreeElementsTest() {
+void InterpolateThreeElementsTest() {
 	std::vector<glm::vec3> result;
-	result = interpolateThreeElementValues(glm::vec3(1.0, 4.0, 9.2), glm::vec3(4.0, 1.0, 9.8), 4);
+	result = InterpolateThreeElementValues(glm::vec3(1.0, 4.0, 9.2), glm::vec3(4.0, 1.0, 9.8), 4);
 	for (size_t i = 0; i < result.size(); i++) std::cout << result[i].r << ", " << result[i].g << ", " << result[i].b << std::endl;
 }
 
@@ -79,8 +242,9 @@ uint32_t PackColour(uint8_t r, uint8_t g, uint8_t b) {
 	return (255 << 24) + (r << 16) + (g << 8) + b;
 }
 
+
 void GrayscaleGradient(DrawingWindow &window) {
-	std::vector<float> row = interpolateSingleFloats(255, 0, window.width);
+	std::vector<float> row = InterpolateSingleFloats(255, 0, window.width);
 	std::vector<uint32_t> rowColours;
 	std::transform(row.cbegin(), row.cend(), std::back_inserter(rowColours), [](float f) -> uint32_t { return PackColour(f, f, f); });
 	for (size_t y = 0; y < window.height; y++) {
@@ -96,54 +260,105 @@ void TwoDGradient(DrawingWindow &window) {
 	glm::vec3 bottomRight(0, 255, 0);    // green 
 	glm::vec3 bottomLeft(255, 255, 0);   // yellow
 
-	std::vector<glm::vec3> left = interpolateThreeElementValues(topLeft, bottomLeft, window.height);
-	std::vector<glm::vec3> right = interpolateThreeElementValues(topRight, bottomRight, window.height);
+	std::vector<glm::vec3> left = InterpolateThreeElementValues(topLeft, bottomLeft, window.height);
+	std::vector<glm::vec3> right = InterpolateThreeElementValues(topRight, bottomRight, window.height);
 	
 	for (size_t y = 0; y < window.height; y++) {
-		std::vector<glm::vec3> row = interpolateThreeElementValues(left[y], right[y], window.width);
+		std::vector<glm::vec3> row = InterpolateThreeElementValues(left[y], right[y], window.width);
 		for (size_t x = 0; x < window.width; x++) {
 			window.setPixelColour(x, y, PackColour(row[x].r, row[x].g, row[x].b));
 		}
 	}
 }
 
-void drawPoints(DrawingWindow &window, std::vector<glm::vec2> points, int colour) {
+CanvasPoint RandomCanvasPoint() {
+	return CanvasPoint(rand() % WIDTH, rand() % HEIGHT);
+}
+
+Colour RandomColour() {
+	return Colour(rand() % 256, rand() % 256, rand() % 256);
+}
+
+void AddRandomTriangle(bool filled, std::vector<Shape2D> &shapes) {
+	if (filled) {
+		std::pair<Shape2D, Shape2D> filled = CreateFilledTriangle2D(
+			CanvasTriangle(
+				RandomCanvasPoint(),
+				RandomCanvasPoint(),
+				RandomCanvasPoint()),
+			RandomColour());
+		shapes.push_back(filled.first);
+		shapes.push_back(filled.second);
+	} 
+	else {
+		shapes.push_back(CreateStrokedTriangle2D(
+			CanvasTriangle(
+				RandomCanvasPoint(),
+				RandomCanvasPoint(),
+				RandomCanvasPoint()),
+			RandomColour()));
+	}
+}
+
+void DrawPoints(DrawingWindow &window, std::vector<glm::vec2> points, int colour) {
 	for (glm::vec2 p : points) {
 		window.setPixelColour(p.x, p.y, colour);
 	}
 }
 
-void DrawLine(DrawingWindow &window, Line2D line) {
-	int col = PackColour(line.colour.red, line.colour.green, line.colour.blue);
-	for (glm::vec2 p : line.points) {
+void DrawTexturedShape2D(DrawingWindow &window, Shape2D shape, TextureMap texture) {
+	for (CanvasPoint p : shape.points) {
+		int pixelIndex = p.texturePoint.y * texture.width + p.texturePoint.x;
+		uint32_t col = texture.pixels.at(pixelIndex);
 		window.setPixelColour(p.x, p.y, col);
 	}
-} 
+}
 
-void witchSymbol(DrawingWindow &window) {
+void DrawShape2D(DrawingWindow &window, Shape2D shape) {
+	int col = PackColour(shape.colour.red, shape.colour.blue, shape.colour.green);
+	for (CanvasPoint p : shape.points) {
+		window.setPixelColour(p.x, p.y, col);
+	}
+}
+
+void WitchSymbol(DrawingWindow &window) {
 	float midX = WIDTH / 2;
 	float midY = HEIGHT / 2;
 	Colour white(255, 255, 255);
-	DrawLine(window, Line2D(glm::vec2(0, 0), glm::vec2(midX, midY), white));
-	DrawLine(window, Line2D(glm::vec2(WIDTH - 1, 0), glm::vec2(midX, midY), white));
-	DrawLine(window, Line2D(glm::vec2(midX, 0), glm::vec2(midX, HEIGHT), white));
-	DrawLine(window, Line2D(glm::vec2(midX - WIDTH / 6, midY), glm::vec2(midX + WIDTH / 6, midY), white));
+	DrawShape2D(window, CreateLine2D(CanvasPoint(0, 0), CanvasPoint(midX, midY), white));
+	DrawShape2D(window, CreateLine2D(CanvasPoint(WIDTH - 1, 0), CanvasPoint(midX, midY), white));
+	DrawShape2D(window, CreateLine2D(CanvasPoint(midX, 0), CanvasPoint(midX, HEIGHT), white));
+	DrawShape2D(window, CreateLine2D(CanvasPoint(midX - WIDTH / 6, midY), CanvasPoint(midX + WIDTH / 6, midY), white));
 }
 
-void draw(DrawingWindow &window) {
+void TestTextureMapping(DrawingWindow &window, TextureMap texture) {
+	CanvasTriangle textureTriangle = CanvasTriangle(CanvasPoint(195, 10), CanvasPoint(395, 380), CanvasPoint(65, 330));
+	CanvasTriangle triangle = CanvasTriangle(CanvasPoint(160, 10), CanvasPoint(300, 230), CanvasPoint(10, 150));
+	Shape2D tri = CreateTexturedTriangle2D(triangle, textureTriangle);
+	DrawTexturedShape2D(window, tri, texture);
+}
+
+void draw(DrawingWindow &window, std::vector<Shape2D> shapes, TextureMap texture) {
 	window.clearPixels();
 	// RedNoise(window);
 	// GrayscaleGradient(window);
 	// TwoDGradient(window);
-	witchSymbol(window);
+	// witchSymbol(window);
+
+	TestTextureMapping(window, texture);
+	for (Shape2D shape : shapes) {
+		DrawShape2D(window, shape);
+	}
 }
 
-void handleEvent(SDL_Event event, DrawingWindow &window) {
+void handleEvent(SDL_Event event, DrawingWindow &window, std::vector<Shape2D> &shapes) {
 	if (event.type == SDL_KEYDOWN) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
 		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
+		else if (event.key.keysym.sym == SDLK_u) AddRandomTriangle(false, shapes);
+		else if (event.key.keysym.sym == SDLK_f) AddRandomTriangle(true, shapes);
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
 		window.saveBMP("output.bmp");
@@ -153,10 +368,13 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
+	std::vector<Shape2D> shapes;
+	std::string filename = "texture.ppm";
+	TextureMap texture = TextureMap(filename);
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
-		if (window.pollForInputEvents(event)) handleEvent(event, window);
-		draw(window);
+		if (window.pollForInputEvents(event)) handleEvent(event, window, shapes);
+		draw(window, shapes, texture);
 		// Need to render the frame at the end, or nothing actually gets shown on the screen !
 		window.renderFrame();
 	}
